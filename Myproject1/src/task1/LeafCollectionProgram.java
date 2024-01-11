d2edea05bc1a19443d90a6d23d6fe8dafd617433
Home <Home@DESKTOP-A93HG4V>
2024-01-11 11:05:37 +0530

    commit

--- Myproject1/src/task1/LeafCollectionProgram.java
+++ Myproject1/src/task1/LeafCollectionProgram.java
@@ -1,10 +1,35 @@
 package task1;
 
+import java.util.Scanner;
+
 public class LeafCollectionProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int totalSamplesCollected = 0;
+        int rounds = 0;
 
-	}
+        System.out.println("Asif is collecting leaf samples in the park.");
 
+        while (true) {
+            System.out.print("Enter the number of leaf samples collected during this round (enter 0 to finish): ");
+            int samplesInThisRound = scanner.nextInt();
+
+            if (samplesInThisRound == 0) {
+                break; // Exit the loop if the user enters 0
+            }
+
+            totalSamplesCollected += samplesInThisRound;
+            rounds++;
+            
+            if (totalSamplesCollected >= 30) {
+                break; // Exit the loop if at least 30 samples are collected
+            }
+        }
+
+        System.out.println("\nAsif completed " + rounds + " rounds around the park.");
+        System.out.println("He collected a total of " + totalSamplesCollected + " leaf samples.");
+
+        scanner.close();
+    }
 }
