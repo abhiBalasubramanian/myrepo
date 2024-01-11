ee73fc2eb0dc0954ad09adee2dc9ae3ad8a25206
Home <Home@DESKTOP-A93HG4V>
2024-01-11 13:13:34 +0530

    commit

---
+++ Myproject1/src/task1/Leaf.java
@@ -0,0 +1,33 @@
+package task1;
+
+public class Leaf {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int totalSamplesCollected = 0;
+        int rounds = 0;
+
+        System.out.println("Asif is collecting leaf samples in the park.");
+
+        while (true) {
+            System.out.print("Enter the number of leaf samples collected during this round: ");
+            int samplesInThisRound = scanner.nextInt();
+
+            if (samplesInThisRound == 0) {
+                break; // Exit the loop if the user enters 0
+            }
+
+            totalSamplesCollected += samplesInThisRound;
+            rounds++;
+            
+            if (totalSamplesCollected >= 30) {
+                break; // Exit the loop if at least 30 samples are collected
+            }
+        }
+
+        System.out.println("\nAsif completed " + rounds + " rounds around the park.");
+        System.out.println("He collected a total of " + totalSamplesCollected + " leaf samples.");
+
+        scanner.close();
+    }
+}
