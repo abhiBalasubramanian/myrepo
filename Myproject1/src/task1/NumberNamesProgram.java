dd1126db000ffca4a315c7926aa2e1e2fcc09142
Home <Home@DESKTOP-A93HG4V>
2024-01-11 11:13:12 +0530

    commit

--- Myproject1/src/task1/NumberNamesProgram.java
+++ Myproject1/src/task1/NumberNamesProgram.java
@@ -1,10 +1,24 @@
 package task1;
 
+import java.util.Scanner;
+
 public class NumberNamesProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.print("Enter a number between 0 and 9: ");
+        int number = scanner.nextInt();
 
-	}
+        if (number >= 0 && number <= 9) {
+            String[] numberNames = {
+                    "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"
+            };
 
+            System.out.println("Number Name: " + numberNames[number]);
+        } else {
+            System.out.println("Please enter a number between 0 and 9.");
+        }
+
+        scanner.close();
+    }
 }
